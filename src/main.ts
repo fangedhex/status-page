2397796c01868168fd585f3096fc784c4f5d1e37
Sylvain VISTE <viste.sylvain@gmail.com>
2021-08-03 22:01:39 +0200

    add logging

--- src/main.ts
+++ src/main.ts
@@ -3,36 +3,34 @@ import pointOfView from "point-of-view";
 import fastifyStatic from "fastify-static";
 import path from "path";
 import { getServices } from "./services";
+import { logger } from "./logger";
 
-// Require the framework and instantiate it
-const server = fastify({
-  logger: true
-})
+const server = fastify();
 
 server.register(pointOfView, {
   engine: {
     twig: require('twig')
   }
-})
+});
 
 server.register(fastifyStatic, {
   root: path.join(__dirname, "../public"),
 });
 
 server.get('/', async (request, reply) => {
-  const data = await getServices();
+  getServices().then((services) => {
+    let groups = Array.from(services).reduce((groups, [key, value]) => (
+      Object.assign(groups, { [key]: value }) // Be careful! Maps can have non-String keys; object literals can't.
+    ), {});
 
-  let groups = Array.from(data).reduce((groups, [key, value]) => (
-    Object.assign(groups, { [key]: value }) // Be careful! Maps can have non-String keys; object literals can't.
-  ), {});
-
-  reply.view('/views/layout.html.twig', {groups});
-})
+    reply.view('/views/layout.html.twig', {groups});
+  }).catch(logger.error);
+});
 
 server.listen(8080, function (err, address) {
   if (err) {
-    server.log.error(err)
-    process.exit(1)
+    logger.error(err);
+    process.exit(1);
   }
-  server.log.info(`server listening on ${address}`)
-})
+  logger.info(`server listening on ${address}`);
+});
